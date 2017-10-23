8ed9d72c94c9c22f84858b54daad677ffed74971
dschma <deborah.scherrerma@epfl.ch>
2017-10-23 12:13:47 +0200

    One Neuron updated : better version, it works

---
+++ One_Neuron/build/CMakeFiles/neuro.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neuro.dir/neurone.cpp.o"
+  "CMakeFiles/neuro.dir/main_neurone.cpp.o"
+  "neuro.pdb"
+  "neuro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neuro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
